c8d5c3fa5d08005649c5aa36643205b46fb018b9
Hung Pham Thanh <thanhhungsky98@gmail.com>
2024-10-22 14:34:58 +0700

    #Code

--- Controllers/MailLogsController.cs
+++ Controllers/MailLogsController.cs
@@ -5,28 +5,86 @@ using Microsoft.EntityFrameworkCore;
 
 namespace mbadmin.Controllers
 {
-    public class MailLogsController : Controller
+    public class LogsController : Controller
     {
         private readonly DatabaseContext _context;
         private const int PageSize = 20;  // 20 rows per page
 
-        public MailLogsController(DatabaseContext context)
+        public LogsController(DatabaseContext context)
         {
             _context = context;
         }
-        public async Task<IActionResult> Index(int page = 1)
-        {
-            // Get the total number of logs
-            int totalLogs = await _context.MailLog.CountAsync();
 
-            // Calculate the logs to skip for pagination
+        // MailLogs
+        public async Task<IActionResult> MailLogs(int page = 1)
+        {
+            int totalLogs = await _context.MailLog.CountAsync();
             var logs = await _context.MailLog
                                      .OrderByDescending(l => l.Time)
                                      .Skip((page - 1) * PageSize)
                                      .Take(PageSize)
                                      .ToListAsync();
 
-            // Pass the logs and pagination info to the view
+            var viewModel = new LogPaginationViewModel
+            {
+                Logs = logs,
+                CurrentPage = page,
+                TotalPages = (int)Math.Ceiling(totalLogs / (double)PageSize)
+            };
+
+            return View(viewModel);
+        }
+
+        // InfoLogs
+        public async Task<IActionResult> InfoLogs(int page = 1)
+        {
+            int totalLogs = await _context.MailLog.CountAsync();
+            var logs = await _context.MailLog
+                                     .OrderByDescending(l => l.Time)
+                                     .Skip((page - 1) * PageSize)
+                                     .Take(PageSize)
+                                     .ToListAsync();
+
+            var viewModel = new LogPaginationViewModel
+            {
+                Logs = logs,
+                CurrentPage = page,
+                TotalPages = (int)Math.Ceiling(totalLogs / (double)PageSize)
+            };
+
+            return View(viewModel);
+        }
+
+        // VideodlLogs
+        public async Task<IActionResult> VideodlLogs(int page = 1)
+        {
+            int totalLogs = await _context.MailLog.CountAsync();
+            var logs = await _context.MailLog
+                                     .OrderByDescending(l => l.Time)
+                                     .Skip((page - 1) * PageSize)
+                                     .Take(PageSize)
+                                     .ToListAsync();
+
+            var viewModel = new LogPaginationViewModel
+            {
+                Logs = logs,
+                CurrentPage = page,
+                TotalPages = (int)Math.Ceiling(totalLogs / (double)PageSize)
+            };
+
+            return View(viewModel);
+        }
+
+        // ScamCheckerLogs
+        public async Task<IActionResult> ScamCheckerLogs(int page = 1)
+        {
+            int totalLogs = await _context.MailLog.CountAsync();
+            var logs = await _context.MailLog
+                                     .OrderByDescending(l => l.Time)
+                                     .Skip((page - 1) * PageSize)
+                                     .Take(PageSize)
+                                     .ToListAsync();
+
             var viewModel = new LogPaginationViewModel
             {
                 Logs = logs,
